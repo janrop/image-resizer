1cfb02d60977aea43e038f37dbf7e264705e6618
Jan Ropertz <ropertz@taktzeit.com>
2018-02-16 17:36:40 +0100

    Funzt mit JPEG und alterscheck

--- resize.go
+++ resize.go
@@ -3,11 +3,16 @@ package main
 import (
 	"errors"
 	"fmt"
+	"image/jpeg"
 	"io/ioutil"
 	"log"
 	"os"
 	"reflect"
 	"strconv"
+	"strings"
+	"time"
+
+	"github.com/nfnt/resize"
 )
 
 func inArray(v interface{}, in interface{}) (ok bool, i int) {
@@ -39,12 +44,10 @@ func inArgs(val string) (ok bool) {
 	return
 }
 
-//const PATH = "resized"
-
 func main() {
 
 	//os.Mkdir(PATH, 0666)
-	_, err := dimensionsInArgs()
+	dimensions, err := dimensionsInArgs()
 	if err != nil {
 		fmt.Println("Usage: ")
 		fmt.Println("  resize 400 # To resize all pictures with a max width and height of 400")
@@ -56,7 +59,7 @@ func main() {
 	check(err)
 
 	for _, dir := range dirs {
-		if !dir.IsDir() {
+		if !dir.IsDir() || dir.Name() == ".git" {
 			continue
 		}
 
@@ -67,31 +70,59 @@ func main() {
 
 		for _, file := range files {
 			if inArgs("-v") {
-				fmt.Println("  Processing file", file.Name())
+				fmt.Println("  Processing file", file.Name(), file.ModTime().Format("Mon 2 Jan 2006 15:04"))
+			}
+
+			if file.ModTime().Unix() > (time.Now().Unix() - 60*60*8) {
+				if inArgs("-v") {
+					fmt.Println("    -> new enough")
+				}
+
+				if strings.HasSuffix(file.Name(), ".JPG") || strings.HasSuffix(file.Name(), ".jpg") || strings.HasSuffix(file.Name(), ".JPEG") || strings.HasSuffix(file.Name(), ".jpeg") {
+					resizeJpeg(dir.Name()+"/originals", file, dimensions)
+				}
 			}
 		}
 
-		// file, err := os.Open(file.Name())
-		// check(err)
-		// defer file.Close()
-
-		// img, err := jpeg.Decode(file)
-		// check(err)
-
-		// newSize := float64(img.Bounds().Size().X) * 0.3
-
-		// m := resize.Resize(uint(newSize), 0, img, resize.Lanczos3)
-
-		// out, err := os.Create(PATH + "/" + file.Name())
-		// check(err)
-		// defer out.Close()
-
-		// jpeg.Encode(out, m, nil)
 	}
+}
+
+func resizeJpeg(folder string, fileInfo os.FileInfo, dimensions int) {
+	file, err := os.Open(folder + "/" + fileInfo.Name())
+	check(err)
+	defer file.Close()
+
+	newFolder := strings.Replace(folder, "originals", strconv.Itoa(dimensions), 1)
+	exists, err := pathExists(newFolder)
+
+	if !exists {
+		os.Mkdir(newFolder, 0766)
+	}
+
+	img, err := jpeg.Decode(file)
+	check(err)
+
+	m := resize.Resize(uint(dimensions), 0, img, resize.Lanczos3)
+	out, err := os.Create(newFolder + "/" + fileInfo.Name())
+	check(err)
+	defer out.Close()
+
+	jpeg.Encode(out, m, nil)
 }
 
 func check(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func pathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
 }
